39b49de8d2cf3bee70b5342e088c8ac487623c2c
Akshay_Jayan <akshay.j@thinkpalm.com>
2023-09-09 10:35:12 +0530

    Add user details api

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {profilePictureUpload} from "../Utils/Upload.js";
-import { Register,Login,editProfile ,getAllusers} from "../Controllers/AuthController.js";
+import { Register,Login,editProfile ,getAllusers,getAUser} from "../Controllers/AuthController.js";
 
 const router= express.Router()
 router.post("/signup",profilePictureUpload.single('profile_picture'),Register);
 router.post("/login", Login);
 router.put("/edit/:userId",profilePictureUpload.single('profile_picture'), editProfile);
 router.get("/users",getAllusers)
+router.get("/user/:userId",getAUser)
 
 export {router as userRouter}
